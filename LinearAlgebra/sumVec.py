65f11a09d89eaa13eb19702660829ee53d1edcb9
akn264 <akn264@nyu.edu>
2019-10-23 21:49:07 -0700

    edit some comments

--- LinearAlgebra/sumVec.py
+++ LinearAlgebra/sumVec.py
@@ -1,4 +1,5 @@
-# compute the sum of two vectors
+# A file that contains ways compute the sum of two vectors
 
+# This is just one way..
 def addn(v, w): return [v[i] + w[i] for i in range(len(v))]
 
